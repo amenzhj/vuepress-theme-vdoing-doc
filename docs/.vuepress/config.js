ce3b72e626b9ab8bf2ad8baed7395ef1037ae840
MrZhou <898013058@qq.com>
2023-03-30 12:00:34 +0800

    代码合并

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,28 +1,69 @@
-const nav = require('./config/nav.js');
-const base = require('../../base.js');
+const baiduCode = require('./config/baiduCode.js'); // 百度统计hm码
 const htmlModules = require('./config/htmlModules.js');
 
+
 module.exports = {
+
+  theme: 'vdoing', // 使用依赖包主题
+  // theme: require.resolve('../../vdoing'), // 使用本地主题 (先将vdoing主题文件下载到本地：https://github.com/xugaoyi/vuepress-theme-vdoing)
+
   title: "vuepress-theme-vdoing",
   description: '一个基于VuePress的 知识管理&博客 主题',
-  base, // '/<仓库名>/'， 默认'/'
+  // base: '/', // 默认'/'。如果你想将你的网站部署到如 https://foo.github.io/bar/，那么 base 应该被设置成 "/bar/",（否则页面将失去样式等文件）
   head: [ // 注入到页面<head> 中的标签，格式[tagName, { attrName: attrValue }, innerHTML?]
     ['link', { rel: 'icon', href: '/img/favicon.ico' }], //favicons，资源放在public文件夹
     ['meta', { name: 'keywords', content: 'vuepress,theme,blog,vdoing' }],
     ['meta', { name: 'theme-color', content: '#11a8cd' }], // 移动浏览器主题颜色
 
     ['meta', { name: 'wwads-cn-verify', content: '6c4b761a28b734fe93831e3fb400ce87' }], // 广告相关，你可以去掉
-    ['script', { src: 'https://cdn.wwads.cn/js/makemoney.js', type: 'text/javascript', async: 'async' }], // 广告相关，你可以去掉
+    ['script', { src: 'https://cdn.wwads.cn/js/makemoney.js', type: 'text/javascript' }], // 广告相关，你可以去掉
   ],
-  markdown: {
-    lineNumbers: true // 代码行号
-  },
-  theme: 'vdoing', // 使用依赖包主题
-  // theme: require.resolve('../../theme-vdoing'), // 使用本地主题
-  themeConfig: { // 主题配置
-    nav,
+
+  // 主题配置
+  themeConfig: {
+    nav: [
+      { text: '首页', link: '/' },
+      {
+        text: '指南', link: '/pages/a2f161/', items: [
+          { text: '主题初衷与诞生', link: '/pages/52d5c3/' },
+          { text: '介绍', link: '/pages/a2f161/' },
+          { text: '快速上手', link: '/pages/793dcb/' },
+          { text: '目录结构', link: '/pages/2f674a/' },
+          { text: '核心配置和约定', link: '/pages/33d574/' },
+          { text: '自动生成front matter', link: '/pages/088c16/' },
+          { text: 'Markdown 容器', link: '/pages/d0d7eb/' },
+          { text: 'Markdown 中使用组件', link: '/pages/197691/' },
+          {
+            text: '相关文章', items: [
+              { text: '使目录栏支持h2~h6标题', link: '/pages/8dfab5/' },
+              { text: '如何让你的笔记更有表现力', link: '/pages/dd027d/' },
+              { text: '批量操作front matter工具', link: '/pages/2b8e22/' },
+              { text: '部署', link: '/pages/0fc1d2/' },
+              { text: '关于写文章和H1标题', link: '/pages/9ae0bd/' },
+              { text: '关于博客搭建与管理', link: '/pages/26997d/' },
+              { text: '在线编辑和新增文章的方法', link: '/pages/c5a54d/' },
+            ]
+          }
+        ]
+      },
+      {
+        text: '配置', link: '/pages/a20ce8/', items: [
+          { text: '主题配置', link: '/pages/a20ce8/' },
+          { text: '首页配置', link: '/pages/f14bdb/' },
+          { text: 'front matter配置', link: '/pages/3216b0/' },
+          { text: '目录页配置', link: '/pages/54651a/' },
+          { text: '添加摘要', link: '/pages/1cc523/' },
+          { text: '修改主题颜色和样式', link: '/pages/f51918/' },
+          { text: '评论栏', link: '/pages/ce175c/' },
+        ]
+      },
+      { text: '资源', link: '/pages/db78e2/' },
+      { text: '案例', link: '/pages/5d571c/' },
+      { text: '问答', link: '/pages/9cc27d/' },
+      { text: '赞助', link: '/pages/1b12ed/' },
+    ],
     sidebarDepth: 2, // 侧边栏显示深度，默认1，最大2（显示到h3标题）
-    logo: 'https://cdn.jsdelivr.net/gh/xugaoyi/image_store/blog/20200409124835.png', // 导航栏logo
+    logo: '/img/logo.png', // 导航栏logo
     repo: 'xugaoyi/vuepress-theme-vdoing', // 导航栏右侧生成Github链接
     searchMaxSuggestions: 10, // 搜索结果显示最大数
     lastUpdated: '上次更新', // 更新的时间，及前缀文字   string | boolean (取值为git提交时间)
@@ -44,14 +85,8 @@ module.exports = {
     //   '图标地址1',
     //   '图标地址2'
     // ],
-    // bodyBgImg: [
-    //   'https://cdn.jsdelivr.net/gh/xugaoyi/image_store/blog/20200507175828.jpeg',
-    //   'https://cdn.jsdelivr.net/gh/xugaoyi/image_store/blog/20200507175845.jpeg',
-    //   'https://cdn.jsdelivr.net/gh/xugaoyi/image_store/blog/20200507175846.jpeg'
-    // ], // body背景大图，默认无。 单张图片 String || 多张图片 Array, 多张图片时每隔15秒换一张。
 
-
-    // categoryText: '随笔', // 碎片化文章（_posts文件夹的文章）预设生成的分类值，默认'随笔'
+    pageStyle: 'line', // 页面风格，可选值：'card'卡片 | 'line' 线（未设置bodyBgImg时才生效）， 默认'card'。 说明：card时背景显示灰色衬托出卡片样式，line时背景显示纯色，并且部分模块带线条边框
 
     // contentBgStyle: 1,
 
@@ -89,34 +124,38 @@ module.exports = {
     },
     htmlModules,
   },
-  plugins: [ // 插件
+
+  // 插件
+  plugins: [
     // [require('./plugins/love-me'), { // 鼠标点击爱心特效
     //   color: '#11a8cd', // 爱心颜色，默认随机色
     //   excludeClassName: 'theme-vdoing-content' // 要排除元素的class, 默认空''
     // }],
 
-    ['thirdparty-search', { // 可以添加第三方搜索链接的搜索框（原官方搜索框的参数仍可用）
-      thirdparty: [ // 可选，默认 []
-        {
-          title: '在GitHub中搜索',
-          frontUrl: 'https://github.com/search?q=', // 搜索链接的前面部分
-          behindUrl: '' // 搜索链接的后面部分，可选，默认 ''
-        },
-        {
-          title: '在npm中搜索',
-          frontUrl: 'https://www.npmjs.com/search?q=',
-        },
-        {
-          title: '在Bing中搜索',
-          frontUrl: 'https://cn.bing.com/search?q='
-        }
-      ]
-    }],
+    ['fulltext-search'], // 全文搜索
+
+    // ['thirdparty-search', { // 可以添加第三方搜索链接的搜索框（原官方搜索框的参数仍可用）
+    //   thirdparty: [ // 可选，默认 []
+    //     {
+    //       title: '在GitHub中搜索',
+    //       frontUrl: 'https://github.com/search?q=', // 搜索链接的前面部分
+    //       behindUrl: '' // 搜索链接的后面部分，可选，默认 ''
+    //     },
+    //     {
+    //       title: '在npm中搜索',
+    //       frontUrl: 'https://www.npmjs.com/search?q=',
+    //     },
+    //     {
+    //       title: '在Bing中搜索',
+    //       frontUrl: 'https://cn.bing.com/search?q='
+    //     }
+    //   ]
+    // }],
 
     [
       'vuepress-plugin-baidu-tongji', // 百度统计
       {
-        hm: '01293bffa6c3962016c08ba685c79d78'
+        hm: baiduCode || '01293bffa6c3962016c08ba685c79d78'
       }
     ],
 
@@ -130,7 +169,7 @@ module.exports = {
       settings: {
         // jsLib: ['http://xxx'], // 在线示例(jsfiddle, codepen)中的js依赖
         // cssLib: ['http://xxx'], // 在线示例中的css依赖
-        // vue: 'https://cdn.jsdelivr.net/npm/vue/dist/vue.min.js', // 在线示例中的vue依赖
+        // vue: 'https://fastly.jsdelivr.net/npm/vue/dist/vue.min.js', // 在线示例中的vue依赖
         jsfiddle: false, // 是否显示 jsfiddle 链接
         codepen: true, // 是否显示 codepen 链接
         horizontal: false // 是否展示为横向样式
@@ -155,12 +194,15 @@ module.exports = {
       }
     ]
   ],
-  // configureWebpack: {
-  //   //webpack别名 如![Image from alias](~@alias/image.png)
-  //   resolve: {
-  //     alias: {
-  //       '@alias': 'path/to/some/dir'
-  //     }
-  //   }
-  // }
+
+  markdown: {
+    // lineNumbers: true,
+    extractHeaders: ['h2', 'h3', 'h4', 'h5', 'h6'], // 提取标题到侧边栏的级别，默认['h2', 'h3']
+  },
+
+  // 监听文件变化并重新构建
+  extraWatchFiles: [
+    '.vuepress/config.js',
+    '.vuepress/config/htmlModules.js',
+  ]
 }
