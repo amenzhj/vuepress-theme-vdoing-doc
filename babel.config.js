2ee7546a1e5b4558956b3477d0a989c1da0bff8e
MrZhou <898013058@qq.com>
2023-03-30 15:18:07 +0800

    修改balel

--- babel.config.js
+++ babel.config.js
@@ -1,18 +1,3 @@
 module.exports = {
-  'env': {
-    'test': {
-      'presets': [
-        ['@babel/preset-env', { 'targets': { 'node': 'current' }}]
-      ],
-      'plugins': [
-        '@babel/plugin-syntax-dynamic-import',
-        [
-          '@babel/plugin-proposal-class-properties',
-          {
-            'loose': true
-          }
-        ]
-      ]
-    }
-  }
+  compact: false,
 }
